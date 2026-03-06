5dc84822c21d05486222f9f20ee67ec49a4941aa
juedra11 <juan1juan.jdar@gmail.com>
2026-03-06 02:53:55 -0500

    Primer commit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    }
+}
